1ae2b5cd6fb0088c9ab732a0bd27c76275bab408
agutierreza <gutialco@gmail.com>
2026-07-07 23:26:08 +0100

    Update tests

--- test/test_collision_detector.cpp
+++ test/test_collision_detector.cpp
@@ -2,6 +2,7 @@
 #include "GravityBilliards/CircleCollisionDetector.hpp"
 #include "GravityBilliards/World.hpp"
 #include "GravityBilliards/Vector2D.hpp"
+#include "GravityBilliards/Topology.hpp"
 
 using namespace GravityBilliards;
 
@@ -10,6 +11,7 @@ protected:
     CircleCollisionDetector detector;
     // Set up a basic world with zero gravity so we isolate collision logic
     World world{0, 1000, 1000}; 
+    EuclideanTopology topology;
 
     void SetUp() override {
         // Clear randomly generated attractors
@@ -28,7 +30,7 @@ TEST_F(CollisionDetectorTest, DefiniteMiss) {
     Vector2D pos(50.0, 50.0);
     double radius = 5.0;
 
-    auto result = detector.checkCollision(world, pos, radius);
+    auto result = detector.checkCollision(world, pos, radius, topology);
     EXPECT_FALSE(result.has_value());
 }
 
@@ -36,7 +38,7 @@ TEST_F(CollisionDetectorTest, DefiniteHit) {
     Vector2D pos(5.0, 0.0);
     double radius = 6.0;
 
-    auto result = detector.checkCollision(world, pos, radius);
+    auto result = detector.checkCollision(world, pos, radius, topology);
     ASSERT_TRUE(result.has_value());
     
     // Normal should point from attractor center to particle center
@@ -49,7 +51,7 @@ TEST_F(CollisionDetectorTest, ExactEdgeTouch) {
     Vector2D pos(15.0, 0.0);
     double radius = 5.0;
 
-    auto result = detector.checkCollision(world, pos, radius);
+    auto result = detector.checkCollision(world, pos, radius, topology);
     EXPECT_FALSE(result.has_value()); // Current baseline uses strictly <
 }
 
@@ -57,7 +59,7 @@ TEST_F(CollisionDetectorTest, AngledHit) {
     Vector2D pos(100.0, 125.0);
     double radius = 10.0;
 
-    auto result = detector.checkCollision(world, pos, radius);
+    auto result = detector.checkCollision(world, pos, radius, topology);
     ASSERT_TRUE(result.has_value());
     
     EXPECT_DOUBLE_EQ(result->normal.x, 0.0);
